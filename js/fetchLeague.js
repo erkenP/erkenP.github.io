04cdb69be66359bf7ce6458a5bbfdba3a14fa256
Erik Augustsson <erik.augustsson@hotmail.com>
2023-04-26 12:53:21 +0200

    jere

--- js/fetchLeague.js
+++ js/fetchLeague.js
@@ -1,8 +1,8 @@
-import { doCORSRequest2, reqType } from './baseRequest.js';
+import { doCORSRequest, reqType } from './baseRequest.js';
 const id = 623400;
 
 const getLeague = () => {
-  const data = doCORSRequest2(`${reqType.leagueClassicStanding}${id}/standings/`);
+  const data = doCORSRequest(`${reqType.leagueClassicStanding}${id}/standings/`);
   document.getElementById("title1").innerHTML = data.standings.results[2].entry;
 }
 
